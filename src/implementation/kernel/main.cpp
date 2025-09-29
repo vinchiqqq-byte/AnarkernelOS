68caeabe9d2251316cbe03b15002f8b26d4f72e6
Винчи <vinchiqqq@gmail.com>
2025-09-29 21:05:00 +0300

    Update main.cpp

--- src/implementation/kernel/main.cpp
+++ src/implementation/kernel/main.cpp
@@ -4,7 +4,7 @@
 extern "C" void kernel_main(){
     print_clear();
     print_set_color(PRINT_COLOR_WHITE, PRINT_COLOR_BLACK);
-    print_str("Welcome to Anarkernel!, ");
+    print_str("Welcome to EntropyWare!, ");
     print_set_color(PRINT_COLOR_YELLOW, PRINT_COLOR_BLACK);
     print_str("ver: pre-release\n");;
     print_set_color(PRINT_COLOR_WHITE, PRINT_COLOR_BLACK);
